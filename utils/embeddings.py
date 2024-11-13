f311e073c76bccfdfac7c1f7d72c83daa2575c85
KorawutMing <6432004421@student.chula.ac.th>
2024-11-13 11:06:35 +0700

    update utility and bert file

--- utils/embeddings.py
+++ utils/embeddings.py
@@ -1,8 +1,17 @@
 from FlagEmbedding import BGEM3FlagModel
+from transformers import AutoTokenizer, BertModel
+import torch
 
-MODEL = BGEM3FlagModel("BAAI/bge-m3", use_fp16=False, device="cuda")
+BGE_M3_MODEL = BGEM3FlagModel("BAAI/bge-m3", use_fp16=False, device="cuda")
+BERT_TOKENIZER = AutoTokenizer.from_pretrained("google-bert/bert-base-uncased")
+BERT_MODEL = BertModel.from_pretrained("google-bert/bert-base-uncased")
 
-def genEmbs(texts: list):
-    embs = MODEL.encode(
+def genEmbsBge(texts: list):
+    embs = BGE_M3_MODEL.encode(
         texts, return_dense=True, return_sparse=True, return_colbert_vecs=False)
     return embs['dense_vecs'], embs['lexical_weights']
+
+def genEmbsBert(texts: list, padding: bool):
+    inputs = BERT_TOKENIZER(texts, return_tensors="pt", padding = padding)
+    outputs = BERT_MODEL(**inputs)
+    return outputs.last_hidden_state
